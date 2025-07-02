c51ece9720065c6de8dab38dc9d7d22a8f6d35dd
Connor Gallopo <connor.gallopo@me.com>
2025-07-02 16:15:37 -0400

    next patch attempt

--- lib/fub_client/her_patch.rb
+++ lib/fub_client/her_patch.rb
@@ -1,80 +1,110 @@
 # Her gem compatibility patch for Faraday 2.x
-# This file must be loaded before the Her gem to prevent NameError
+# This file intercepts Her gem loading to prevent Faraday::Response::Middleware errors
 
-# Create the missing Faraday::Response::Middleware class that Her expects
+# First, try to create the missing Faraday::Response::Middleware class using const_set
 if defined?(Faraday) && !defined?(Faraday::Response::Middleware)
-  Faraday::Response.class_eval do
-    class Middleware < Faraday::Middleware
-      def initialize(app, options = {})
-        super(app)
-        @options = options
-      end
+  middleware_class = Class.new(Faraday::Middleware) do
+    def initialize(app, options = {})
+      super(app)
+      @options = options
     end
   end
-  puts "[FubClient] Created Faraday::Response::Middleware for Her gem compatibility (her_patch)"
+  
+  Faraday::Response.const_set('Middleware', middleware_class)
+  puts "[FubClient] Created Faraday::Response::Middleware using const_set"
 end
 
-# Also monkey patch the specific Her middleware classes to avoid the inheritance issue
-module Her
-  module Middleware
-    # Redefine ParseJSON to inherit from Faraday::Middleware directly
-    class ParseJSON < Faraday::Middleware
-      def initialize(app, options = {})
-        super(app)
-        @options = options
-      end
+# Override require to intercept Her middleware loading
+original_require = method(:require)
 
-      def on_complete(env)
-        if process_response_type?(env[:response_headers]['content-type'])
-          env[:body] = parse(env[:body])
+define_method(:require) do |name|
+  case name
+  when 'her/middleware/parse_json'
+    # Define Her::Middleware::ParseJSON without loading the original file
+    module Her
+      module Middleware
+        class ParseJSON < Faraday::Middleware
+          def initialize(app, options = {})
+            super(app)
+            @options = options
+          end
+
+          def on_complete(env)
+            if process_response_type?(env[:response_headers]['content-type'])
+              env[:body] = parse(env[:body])
+            end
+          end
+
+          private
+
+          def parse(body)
+            MultiJson.load(body, symbolize_keys: true)
+          rescue MultiJson::ParseError => e
+            {
+              error: "JSON parsing error: #{e.message}",
+              body: body
+            }
+          end
+
+          def process_response_type?(content_type)
+            content_type && content_type.match(/application\/json/)
+          end
         end
       end
-
-      private
-
-      def parse(body)
-        MultiJson.load(body, symbolize_keys: true)
-      rescue MultiJson::ParseError => e
-        {
-          error: "JSON parsing error: #{e.message}",
-          body: body
-        }
-      end
-
-      def process_response_type?(content_type)
-        content_type && content_type.match(/application\/json/)
-      end
     end
+    puts "[FubClient] Intercepted and replaced her/middleware/parse_json"
+    true
+  when 'her/middleware/first_level_parse_json'
+    # Define Her::Middleware::FirstLevelParseJSON without loading the original file
+    module Her
+      module Middleware
+        class FirstLevelParseJSON < Faraday::Middleware
+          def initialize(app, options = {})
+            super(app)
+            @options = options
+          end
 
-    # Redefine FirstLevelParseJSON to inherit from Faraday::Middleware directly
-    class FirstLevelParseJSON < Faraday::Middleware
-      def initialize(app, options = {})
-        super(app)
-        @options = options
-      end
+          def on_complete(env)
+            if process_response_type?(env[:response_headers]['content-type'])
+              env[:body] = parse(env[:body])
+            end
+          end
 
-      def on_complete(env)
-        if process_response_type?(env[:response_headers]['content-type'])
-          env[:body] = parse(env[:body])
+          private
+
+          def parse(body)
+            MultiJson.load(body)
+          rescue MultiJson::ParseError => e
+            {
+              error: "JSON parsing error: #{e.message}",
+              body: body
+            }
+          end
+
+          def process_response_type?(content_type)
+            content_type && content_type.match(/application\/json/)
+          end
         end
       end
-
-      private
-
-      def parse(body)
-        MultiJson.load(body)
-      rescue MultiJson::ParseError => e
-        {
-          error: "JSON parsing error: #{e.message}",
-          body: body
-        }
-      end
-
-      def process_response_type?(content_type)
-        content_type && content_type.match(/application\/json/)
+    end
+    puts "[FubClient] Intercepted and replaced her/middleware/first_level_parse_json"
+    true
+  when /^her\/middleware\//
+    # For any other Her middleware files, try to load them normally but catch errors
+    begin
+      original_require.call(name)
+    rescue NameError => e
+      if e.message.include?('Faraday::Response::Middleware')
+        puts "[FubClient] Skipped loading #{name} due to Faraday::Response::Middleware error"
+        true
+      else
+        raise e
       end
     end
+  else
+    # For all other requires, use the original method
+    original_require.call(name)
   end
 end
 
-puts "[FubClient] Applied Her gem compatibility patches"
+puts "[FubClient] Applied Her gem require interception patches"
